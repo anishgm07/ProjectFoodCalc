edce89f3199a7f1f620add6ed409aff0bff3445c
anishgm07 <anishgm007@gmail.com>
2025-01-15 17:51:55 +0900

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,37 +9,6 @@
 
     });
 
-
-	// $(window).scroll(function() {
-	//   var scroll = $(window).scrollTop();
-	//   var box = $('.header-text').height();
-	//   var header = $('header').height();
-
-	//   if (scroll >= box - header) {
-	//     $("header").addClass("background-header");
-	//   } else {
-	//     $("header").removeClass("background-header");
-	//   }
-	// })
-
-	$('.owl-banner').owlCarousel({
-	  center: true,
-      items:1,
-      loop:true,
-      nav: true,
-	  dots:true,
-	  navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
-      margin:30,
-      responsive:{
-        992:{
-            items:1
-        },
-		1200:{
-			items:1
-		}
-      }
-	});
-
 	var width = $(window).width();
 		$(window).resize(function() {
 		if (width > 767 && $(window).width() < 767) {
@@ -49,30 +18,6 @@
 			location.reload();
 		}
 	})
-
-	const elem = document.querySelector('.properties-box');
-	const filtersElem = document.querySelector('.properties-filter');
-	if (elem) {
-		const rdn_events_list = new Isotope(elem, {
-			itemSelector: '.properties-items',
-			layoutMode: 'masonry'
-		});
-		if (filtersElem) {
-			filtersElem.addEventListener('click', function(event) {
-				if (!matchesSelector(event.target, 'a')) {
-					return;
-				}
-				const filterValue = event.target.getAttribute('data-filter');
-				rdn_events_list.arrange({
-					filter: filterValue
-				});
-				filtersElem.querySelector('.is_active').classList.remove('is_active');
-				event.target.classList.add('is_active');
-				event.preventDefault();
-			});
-		}
-	}
-
 
 	// Menu Dropdown Toggle
 	if($('.menu-trigger').length){
